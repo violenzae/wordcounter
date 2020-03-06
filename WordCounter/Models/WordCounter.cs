cf96b42ad18c98b070287ffc9f17e7ed9b06459b
Rachel Schieferstein <violenza@gmail.com>
2020-03-06 08:55:31 -0800

    adding initial folder structure skeleton

---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace WordCounter.Models
+{
+  public class Word
+  {
+    
+  }
+}
---
+++ WordCounter/Program.cs
@@ -0,0 +1,16 @@
+
+using System.Collections.Generic;
+using WordCounter.Models;
+using System;
+
+namespace WordCounter
+{
+  public class Program
+  {
+    static void Main()
+    {
+
+
+    }
+  }
+}
