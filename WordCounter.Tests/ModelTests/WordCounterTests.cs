b35e23b207bef1bcc9306a7c249e2802e8a73e47
Rachel Schieferstein <violenza@gmail.com>
2020-03-06 10:06:27 -0800

    running and failing first test

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -6,6 +6,13 @@ namespace WordCounter.Tests
   [TestClass]
   public class WordTest
   {
-    
+    [TestMethod]
+    public void Counter_ReturnIntOfOccurences_int()
+    {
+      string word = "a";
+      string sample = "a A a";
+
+      Assert.AreEqual(3, Word.Counter(word, sample));
+    }
   }
 }
